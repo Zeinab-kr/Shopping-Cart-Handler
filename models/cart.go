2764d5f02b3b09e450068afaeacf2271a98cbad3
Zeinab-kr <zeinab_kr@aut.ac.ir>
2024-05-29 18:52:03 +0330

    shopping cart model added

--- models/cart.go
+++ models/cart.go
@@ -4,15 +4,13 @@ import (
 	"gorm.io/gorm"
 )
 
-// Cart represents a shopping cart
 type Cart struct {
 	gorm.Model
-	UserID uint   `gorm:"not null"` // Link to the User model
+	UserID uint   `gorm:"not null"`
 	Data   string `gorm:"type:varchar(2048)" validate:"max=2048"`
 	State  string `gorm:"type:varchar(10)" validate:"customState"`
 }
 
-// CartInput is a struct used to represent the input data for a cart
 type CartInput struct {
 	Data  string
 	State string
